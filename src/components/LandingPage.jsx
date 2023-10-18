8ef9bca037192bd12af9c692f15f9eb881a60f2d
Odartei Lamptey <odarteilamptey.ml@gmail.com>
2023-10-18 16:02:13 +0100

    tailwin updates

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { useAuth0 } from "@auth0/auth0-react";
 
 export default function LandingPage() {
@@ -7,16 +6,16 @@ export default function LandingPage() {
 
   return (
     <>
-        <div className="landing-container">
-            <div className="image-side">
-                <img src="https://res.cloudinary.com/dlcqoiyb1/image/upload/v1697134707/samples/cup-on-a-table.jpg" alt="image" width={'100%'} height={'100%'} />
+        <div className="landing-container w-full md:flex md:px-[10%] md:mt-[5%]  ">
+            <div className="image-side w-full  h-[100%] md:w-[50%] md:h-[500px] md:rounded-tl-md md:rounded-bl-md ">
+                <img className=" h-full md:rounded-tl-md md:rounded-bl-md" src="https://res.cloudinary.com/dlcqoiyb1/image/upload/v1697134707/samples/cup-on-a-table.jpg" alt="image" width={'100%'} />
             </div>
-            <div className="text-side">
-                <h3>Welcome</h3>
-                <h1>POWER LIST <br /><span>APP</span></h1>
+            <div className="text-side w-full md:w-[50%] rounded-l-none rounded-r-md bg-[#012E40] text-white p-[40px] ">
+                <h3 className=" font-thin italic text-[30px] mb-7  ">Welcome</h3>
+                <h1 className=" font-extrabold text-[70px] md:text-[50px] ">POWER LIST <br /><span>APP</span></h1>
 
                 <div className="start">
-                <button onClick={() => loginWithRedirect()}>Get Started</button>
+                <button onClick={() => loginWithRedirect()}>Get Started</button> 
             </div>
             </div>
         </div>
